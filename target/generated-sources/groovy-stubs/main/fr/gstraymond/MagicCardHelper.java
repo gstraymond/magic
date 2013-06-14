a57050c4ddb52c1f7d88c476eb76a823d9023323
guillaume <guillaume@gstraymond.fr>
2013-06-14 21:18:26 +0200

    Déplacement des fichiers à la racine

---
+++ target/generated-sources/groovy-stubs/main/fr/gstraymond/MagicCardHelper.java
@@ -0,0 +1,23 @@
+package fr.gstraymond;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class MagicCardHelper
+  extends java.lang.Object  implements
+    groovy.lang.GroovyObject {
+public MagicCardHelper
+(java.util.List cardAsString) {}
+public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
+public  void setMetaClass(groovy.lang.MetaClass mc) { }
+public  java.lang.Object invokeMethod(java.lang.String method, java.lang.Object arguments) { return null;}
+public  java.lang.Object getProperty(java.lang.String property) { return null;}
+public  void setProperty(java.lang.String property, java.lang.Object value) { }
+public  java.util.List getCardAsString() { return (java.util.List)null;}
+public  void setCardAsString(java.util.List value) { }
+public  fr.gstraymond.MagicCard parseCard() { return (fr.gstraymond.MagicCard)null;}
+}
---
+++ target/generated-sources/groovy-stubs/main/magic.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class magic
+  extends groovy.lang.Script {
+public magic
+() {}
+public magic
+(groovy.lang.Binding context) {}
+public static  void main(java.lang.String... args) { }
+public  java.lang.Object run() { return null;}
+}
